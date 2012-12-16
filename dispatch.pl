692e8f2cb2e37c788520f795893d88643bcc4ffa
Hike Danakian <hdana2@gmail.com>
2012-12-16 05:42:51 -0800

    Significant code cleanup and improvement

--- dispatch.pl
+++ dispatch.pl
@@ -1,60 +1,42 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use strict;
 use warnings;
 use File::Basename;
-use Env '@PATH';
 
-my $default = 'help';  # set this to the command to run if one isn't provided
-my $fallback = 'git';  # set this to your program's actual name
-local $0 = ($0 and $0 ne '-') ? basename($0) : $fallback;
-my $prefix = "/usr/libexec/$0";  # set this to where the target binaries live
-my $command = $default;
-if (@ARGV and $ARGV[0] !~ /^-/) {
-    $command = shift @ARGV;
-}
+my $def_cmd= 'help';
+local $0 = ($0 and $0 ne '-') ? basename($0)
+                              : die "Multi-exec command called with no basename\n";
+my @path = ("/usr/libexec/$0", split ':', $ENV{PATH});
+my $cmd = (@ARGV and $ARGV[0] !~ /^-/) ? shift @ARGV : $def_cmd;
 
-# Set your aliases here, or maybe import it from a config file. Values should
-# be either a command to run, or a perl subroutine, with the command-line args
-# passed in. The args will not be appended if the string value ends in
-# a newline. It is up to you to insert $0 or "$prefix/$0-" if desired.
+# An alias can be a shell command or a subroutine. Note that @ARGV holds
+# whatever command-line args were passed, and $0 holds the command basename.
 my %alias = (
-# e.g.
-#    help   => "echo 'rtfm'\n"  # runs "echo 'rtfm'"
-#    stat   => "$0 status"  # runs "/usr/libexec/git status args..."
-#    access => sub { die "Access Denied!\n" if $_[0] != 'secret' }
+    help   => sub {
+        print "Possible commands:\n", map {"\t$_\n"} find_subcmds();
+    },
 );
 
-my $exec_cmd;
-if (exists $alias{$command}) {
-    if ('CODE' eq ref($alias{$command})) {
-        exit($alias{$command}->(@ARGV))
-    } elsif (chomp $alias{$command}) {
-        $exec_cmd = $alias{$command}
+if (exists $alias{$cmd}) {
+    if ('CODE' eq ref($alias{$cmd})) {
+        exit($alias{$cmd}->())
     } else {
-        $exec_cmd = "$alias{$command} @ARGV"
+        exec $alias{$cmd}
     }
 } else {
-    $exec_cmd = find_best_exec("$0-$command", $prefix) . " @ARGV"
-}
-
-print $exec_cmd;
-exec $exec_cmd;
-
-# find the best exec candidate, even with a file extension
-sub find_best_exec {
-    my ($filename, $prefix) = @_;
-    my $found;
-    for ($prefix, @PATH) {
-        $found = best_file_in_dir($filename, $_);
-        last if $found;
+    # find the best exec candidate, even with a file extension
+    # fall back to the bare command "$base-$cmd"
+    for (@path) {
+        opendir(my $dh, $_) or next;
+        my @files = sort grep {/^$0-$cmd\.?/} readdir $dh;
+        closedir $dh;
+        exec ("$_/$files[0]", @ARGV) if @files
     }
-    return $found if $found;
-    return $filename;
+    exec "$0-$cmd";
 }
-sub best_file_in_dir {
-    my ($filename, $dir) = @_;
-    opendir(my $dh, $dir) or return;
-    my @files = sort grep {/^$filename\.?/} readdir $dh;
-    closedir $dh;
-    return "$dir/$files[0]" if @files;
+sub find_subcmds {
+    (
+        keys(%alias),
+        map { m|/$0-([^/]+)$| } map { glob("$_/$0-*") } @path
+    )
 }
