5a4ac2cba470f3dac365ccec8e1dcf8d50ba3257
Hike Danakian <hike@hdanak.com>
2013-03-09 11:52:11 -0800

    reduced code and simplified interface

--- lib/Dispatch/GitLike.pm
+++ lib/Dispatch/GitLike.pm
@@ -4,50 +4,38 @@ use File::Basename;
 
 sub new {
     my ($class, %opts) = @_;
+    my @path = defined($opts{path}) ? @{$opts{path}} : split(':', $ENV{PATH});
+    my $base = $opts{base} // ( ($0 and $0 ne '-') ? basename($0)
+                           : die "Error: Cannot resolve command basename.\n" );
     my $self = bless {
-        default => $opts{default} // 'help',
-        alias   => {
+        default  => $opts{default} // 'help',
+        commands => {
             help   => sub {
                 my $self = shift;
                 print STDERR "Possible commands:\n",
-                                map {"\t$_\n"} $self->find_commands();
+                                map {"\t$_\n"} keys %{$$self{commands}};
             },
-            %{ $opts{alias} // {} }
+            %{ $opts{commands} // {} },
+            map {
+                my $cmdpath = $_;
+                m|/$base-([^/]+)$| ? ( $1 => sub {
+                    my $self = shift;
+                    exec($cmdpath, @{$$self{argv}})
+                } ) : ()
+            } sort grep { -x } map { glob "$_/$base-*" } @path
         },
-        path    => [ split ':', $ENV{PATH} ],
-        base    => $opts{base} // (($0 and $0 ne '-') ? basename($0) :
-                                die "Error: Cannot resolve command basename.\n"),
+        path    => \@path,
+        base    => $base,
     }, $class;
-    my @argv = @ARGV;
-    $$self{cmd} = (@argv and $argv[0] !~ /^-/) ? shift(@argv) : $$self{default};
-    $$self{argv} = \@argv;
     return $self;
 }
 sub run {
-    my ($self, $cmd, $argv) = shift;
-    my $alias = $$self{alias};
-    $$self{cmd}  = $cmd  // $$self{cmd};
-    $$self{argv} = $argv // $$self{argv};
-    if (exists $$alias{$cmd}) {
-        ('CODE' eq ref($$alias{$cmd})) ? exit($$alias{$cmd}->($self))
-                                       : exec($$alias{$cmd}, @$argv)
-    } else {
-        # Find the best exec candidate, with any file extension.
-        for (@$path) {
-            opendir(my $dh, $_) or next;
-            my @files = sort grep {/^$base-$cmd\.?/} readdir($dh);
-            closedir($dh);
-            exec("$_/$files[0]", @$argv) if @files
-        }
-        die "Error: Could not resolve command $base-$cmd.\n";
-    }
-}
-sub commands {
-    my $self = shift;
-    return (
-        keys(%{$$self{alias}}),
-        map { m|/$$self{base}-([^/]+)$| } map { glob("$_/$$self{base}-*") } @{$$self{path}}
-    )
+    my ($self, @argv) = shift;
+    my $cmd = (@argv and $argv[0] !~ /^-/) ? shift(@argv) : $$self{default};
+    die "Error: Could not resolve command '$$self{base} $cmd'.\n"
+            unless exists $$commands{$cmd};
+    $$self{qw[cmd argv]} = ( $cmd, \@argv );
+    exit($$commands{$cmd}->($self))
 }
 
 1
